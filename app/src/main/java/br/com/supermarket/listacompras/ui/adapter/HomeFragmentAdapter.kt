a6509aa6ecb8f45826a5095e3deb66a9f8a5fb1f
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2021-10-05 14:47:10 -0300

    Configurado a recyclerview e o adapter

---
+++ app/src/main/java/br/com/supermarket/listacompras/ui/adapter/HomeFragmentAdapter.kt
@@ -0,0 +1,56 @@
+package br.com.supermarket.listacompras.ui.adapter
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import br.com.supermarket.listacompras.databinding.HomeFragmentBinding
+import br.com.supermarket.listacompras.databinding.ItemListaBinding
+import br.com.supermarket.listacompras.model.ItemDeCompra
+
+class HomeFragmentAdapter(
+    private val context: Context
+): ListAdapter<ItemDeCompra, HomeFragmentAdapter.ViewHolder>(DiffCallback) {
+    init {
+        Log.i("Adapter","init chamado")
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater= LayoutInflater.from(context)
+        val viewBinding = ItemListaBinding.inflate(inflater, parent, false)
+//        return ViewHolder.from(parent)
+        return ViewHolder(viewBinding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        getItem(position)?.let { item ->
+            holder.bind(item)
+        }
+    }
+
+    class ViewHolder(private val binding: ItemListaBinding) :
+        RecyclerView.ViewHolder(binding.root)
+    {
+        fun bind(
+            item: ItemDeCompra
+        ){
+            binding.nomeItem.text = item.nome
+        }
+        companion object{
+            fun from(parent: ViewGroup): ViewHolder{
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemListaBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+
+}
+
+object DiffCallback: DiffUtil.ItemCallback<ItemDeCompra>(){
+    override fun areItemsTheSame(oldItem: ItemDeCompra, newItem: ItemDeCompra) = oldItem.id == newItem.id
+    override fun areContentsTheSame(oldItem: ItemDeCompra, newItem: ItemDeCompra) = oldItem == newItem
+}
--- app/src/main/java/br/com/supermarket/listacompras/ui/fragment/HomeFragment.kt
+++ app/src/main/java/br/com/supermarket/listacompras/ui/fragment/HomeFragment.kt
@@ -5,16 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import br.com.supermarket.listacompras.databinding.HomeFragmentBinding
+import br.com.supermarket.listacompras.ui.adapter.HomeFragmentAdapter
+import br.com.supermarket.listacompras.ui.viewmodel.ListaDeComprasViewModel
+import kotlinx.android.synthetic.main.home_fragment.*
+import org.koin.android.ext.android.inject
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class HomeFragment: Fragment() {
+
+    val view: ListaDeComprasViewModel by viewModel()
+    val listaAdapter: HomeFragmentAdapter by inject()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = HomeFragmentBinding.inflate(inflater)
+
+        val binding = HomeFragmentBinding.inflate(inflater, container, false)
+
+        binding.lifecycleOwner = this
+        binding.recyclerViewHome.adapter = listaAdapter
+
+
+
+        view.todas.observe(requireActivity(), Observer {
+            listaAdapter.submitList(it)
+        })
+
         return binding.root
     }
 }
